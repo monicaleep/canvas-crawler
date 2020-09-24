61296dc87d1533705e96d154062c6f7907326a2d
monicaleep <monica.parrillo@gmail.com>
2020-09-23 21:48:47 -0500

    add key handlers

--- js/main.js
+++ js/main.js
@@ -29,23 +29,82 @@ const drawBox = (x,y,size, color) =>{
 // ctx.fillRect(10,10,100,100)
 // // call stokeRect to draw the line rectangle
 // ctx.strokeRect(10,10,100,100)
-game.addEventListener('click',(e)=>{
-  ogre.x = e.offsetX;
-  ogre.y = e.offsetY;
-  ogre.render()
-})
 
-// Character refs
-let ogre = {
-  x: 500,
-  y: 500,
-  color: '#bada55',
-  width: 40,
-  height: 80,
-  alive: true,
-  render : function(){
-    console.log('test')
+// crawler constructor function
+function Crawler(x,y,width,height,color){
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.color = color;
+  this.alive = true;
+  this.render = function(){
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x,this.y,this.width,this.height)
+  };
+  return this;
+}
+// character refs
+let ogre = new Crawler(300,100,80,120,'#bada55');
+let hero = new Crawler(20,100,50,50,'hotpink')
+
+const gameLoop = () => {
+  // clear the Canvas
+  ctx.clearRect(0,0,game.width,game.height)
+  // display the x,y coordinates of our hero onto the DOM
+  movementDisplay.innerText = `X:${hero.x}\nY:${hero.y}`
+  // check if the ogre is alive and
+  if(ogre.alive){
+    // if so, render the ogre
+    ogre.render()
+  }
+  // check for collision todo
+  // render the hero
+  hero.render()
+}
+
+const movementHandler = (e) =>{
+  // when user presses w, hero moves up
+  if(e.keyCode === 87 && hero.y>0){
+      hero.y = hero.y - 5;
+    //a moves left
+  } else if (e.keyCode === 65 && hero.x>0){
+      hero.x -= 5;
+    // s is down 83
+  } else if (e.keyCode === 83 && (hero.y+hero.height)<game.height){
+      hero.y += 5;
+    //d is right 68
+  } else if (e.keyCode === 68 && (hero.x+hero.width)<game.width){
+      hero.x += 5
   }
 }
+
+document.addEventListener('keydown',movementHandler)
+// game.addEventListener('click',(e)=>{
+//   // clear the gameboard
+//   ctx.clearRect(0,0,game.width,game.height)
+//   // render our hero
+//   hero.render()
+//   // move the ogre
+//   ogre.x = e.offsetX;
+//   ogre.y = e.offsetY;
+//   //render the ogre
+//   ogre.render()
+// })
+
+let runGame = setInterval(gameLoop, 60)
+
+// Character refs
+// let ogre = {
+//   x: 500,
+//   y: 500,
+//   color: '#bada55',
+//   width: 40,
+//   height: 80,
+//   alive: true,
+//   render : function(){
+//     console.log('test')
+//     ctx.fillStyle = this.color;
+//     ctx.fillRect(this.x,this.y,this.width,this.height)
+//   }
+// }
